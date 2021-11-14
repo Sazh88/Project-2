f4402bd6cfb0819a3ac669fbd0f39dff6ee633f7
Sazh88 <86631783+Sazh88@users.noreply.github.com>
2021-11-14 12:52:10 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,41 +1,25 @@
-const path = require('path');
 const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const app = express();
-const PORT = process.env.PORT || 3001;
+const bodyParser = require('body-parser');
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+  
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+  });
+  
+app.post('/login', (req, res) => {
+  
+    let username = req.body.username;
+    let password = req.body.password;
+    res.send(`Username: ${username} Password: ${password}`);
+  });
 
-app.use(session(sess));
+  const port = 3000 
 
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+  app.listen(port, () => console.log(`This app is listening on port ${port}`));
+  
