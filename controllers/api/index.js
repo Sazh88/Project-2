ca36e7621c64a74b01c21723e2cccb8641e9a80c
Ashleighw23 <ashleigh_williams23@hotmail.com>
2021-11-16 20:56:47 -0800

    added email invite

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const emailRoutes = require('./email-routes.js');
 
 router.use('/user', userRoutes);
-router.use('/post', postRoutes);
-router.use('/comment', commentRoutes);
+router.use('/email', emailRoutes);
 
 module.exports = router;
